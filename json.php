e7058adf6e1c084bc51b9e6e07556b4126f066f5
atcherdsd <94963661+atcherdsd@users.noreply.github.com>
2023-03-14 10:04:20 +0300

    Update json.php

--- json.php
+++ json.php
@@ -1,7 +1,4 @@
 <?php
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Origin: http://127.0.0.1:3000");
-header("Access-Control-Allow-Methods: GET,POST,OPTIONS,DELETE,PUT");
 $data = json_decode(file_get_contents("php://input"));
 echo "Registration of $data->lastName completed successfully";
