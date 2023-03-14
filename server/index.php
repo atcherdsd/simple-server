a92ff8a3c367ce7d2f2ef0b1390e8a9d6740dd5c
atcherdsd <94963661+atcherdsd@users.noreply.github.com>
2023-03-14 11:07:29 +0300

    Update index.php

--- server/index.php
+++ server/index.php
@@ -1,4 +1,5 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 $data = json_decode(file_get_contents("php://input"));
 echo "Registration completed successfully";
